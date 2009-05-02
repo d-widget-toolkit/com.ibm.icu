611d317169a33bda76dae99c58e80f34c75e0dd3
Frank Benoit <benoit@tionex.de>
2009-05-02 11:27:24 +0200

    More work on icu

--- src/com/ibm/icu/text/DateFormat.d
+++ src/com/ibm/icu/text/DateFormat.d
@@ -5,15 +5,53 @@ import java.text.ParsePosition;
 import java.util.Date;
 
 import com.ibm.icu.mangoicu.UDateFormat;
+import com.ibm.icu.mangoicu.ULocale;
+import com.ibm.icu.mangoicu.UTimeZone;
 
 public class DateFormat {
 
-    public static const int LONG = UDateFormat.Style.Long;
-    public static const int FULL = UDateFormat.Style.Full;
-    public static const int SHORT = UDateFormat.Style.Short;
+    public static const int LONG   = UDateFormat.Style.Long;
+    public static const int FULL   = UDateFormat.Style.Full;
+    public static const int SHORT  = UDateFormat.Style.Short;
     public static const int MEDIUM = UDateFormat.Style.Medium;
 
+    private UDateFormat ufmt;
+    private this( UDateFormat.Style time ){
+        ufmt = new UDateFormat( time, time, ULocale.Default, UTimeZone.Default, null );
+    }
+    private this( UDateFormat ufmt ){
+        this.ufmt = ufmt;
+    }
+
+    public static DateFormat getTimeInstance() {
+        // FIXME
+        return new DateFormat( UDateFormat.Style.Long );
+    }
+
+    public static DateFormat getTimeInstance(int s) {
+        // FIXME
+        return new DateFormat( UDateFormat.Style.Long );
+        implMissing(__FILE__, __LINE__);
+        return null;
+    }
+
     public static DateFormat getDateInstance() {
+        // FIXME
+        return new DateFormat( UDateFormat.Style.Long );
+        implMissing(__FILE__, __LINE__);
+        return null;
+    }
+
+    public static DateFormat getDateTimeInstance(int l, int m) {
+        // FIXME
+        return new DateFormat( UDateFormat.Style.Long );
+        implMissing(__FILE__, __LINE__);
+        return null;
+    }
+
+    public static DateFormat getDateInstance(int dateFormat) {
+        // FIXME
+        return new DateFormat( UDateFormat.Style.Long );
         implMissing(__FILE__, __LINE__);
         return null;
     }
@@ -23,32 +61,12 @@ public class DateFormat {
         return null;
     }
 
-    public static DateFormat getTimeInstance() {
-        implMissing(__FILE__, __LINE__);
-        return null;
-    }
-
-    public static DateFormat getDateTimeInstance(int l, int m) {
-        implMissing(__FILE__, __LINE__);
-        return null;
-    }
-
-    public static DateFormat getTimeInstance(int s) {
-        implMissing(__FILE__, __LINE__);
-        return null;
-    }
-
-    public static DateFormat getDateInstance(int dateFormat) {
+    public String format(Long long2) {
         implMissing(__FILE__, __LINE__);
         return null;
     }
 
     public Date parse(String str, ParsePosition pos) {
-        implMissing(__FILE__, __LINE__);
-        return null;
-    }
-
-    public String format(Long long2) {
         implMissing(__FILE__, __LINE__);
         return null;
     }
--- src/com/ibm/icu/text/SimpleDateFormat.d
+++ src/com/ibm/icu/text/SimpleDateFormat.d
@@ -2,11 +2,20 @@
 module com.ibm.icu.text.SimpleDateFormat;
 import com.ibm.icu.text.DateFormat;
 
+import com.ibm.icu.mangoicu.UDateFormat;
+import com.ibm.icu.mangoicu.ULocale;
+import com.ibm.icu.mangoicu.UTimeZone;
+
 import java.lang.all;
 public class SimpleDateFormat : DateFormat {
 
     public this(String string) {
         implMissing(__FILE__, __LINE__);
+        super( new UDateFormat(
+                    UDateFormat.Style.Default,
+                    UDateFormat.Style.Default,
+                    ULocale.Default,
+                    UTimeZone.Default, null ));
     }
 
 }
